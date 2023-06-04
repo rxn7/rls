ecdda1fcf8172c640b07a87d425ef62a54635771
rxn7 <rotthin_dev@protonmail.com>
2023-06-04 09:30:06 +0200

    Put a separator only before an entry and if it's not the first entry

--- rls.c
+++ rls.c
@@ -1,24 +1,30 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <dirent.h>
 
 int main(int argc, const char **argv) {
-        char cwd[4096];
-        getcwd(cwd, 4096);
+  char cwd[4096];
+  getcwd(cwd, 4096);
 
-        DIR *dir = opendir(cwd);
-        if(!dir) {
-                printf("Failed to open current working directory!\n");
-                return -1;
-        }
+  DIR *dir = opendir(cwd);
+  if (!dir) {
+    fprintf(stderr, "Failed to open current working directory!\n");
+    return -1;
+  }
 
-        struct dirent *entry;
-        while((entry = readdir(dir)) != NULL) {
-                if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-                        printf("%s | ", entry->d_name);
-                }
-        }
-        
-        closedir(dir);
+  unsigned int i = 0;
+  struct dirent *entry;
+  while ((entry = readdir(dir)) != NULL) {
+    if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+      if (i != 0)
+        fputs(" | ", stdout);
+
+      printf("%s", entry->d_name);
+    }
+
+    ++i;
+  }
+
+  closedir(dir);
 }
