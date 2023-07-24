c19a0715a93384a94adec853c36d1585aac84492
rxn7 <rotthin_dev@protonmail.com>
2023-07-24 03:52:07 +0200

    Added -a flag, path is now relative

--- rls.c
+++ rls.c
@@ -1,15 +1,51 @@
 #include <dirent.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+
+typedef struct {
+  bool hidden_files;
+} flags_t;
+
+void evaluate_flags(flags_t *flags, const char *str) {
+  for (int j = 1; j < strlen(str); ++j) {
+    const char c = str[c];
+    switch (c) {
+    case 'a':
+      flags->hidden_files = true;
+      break;
+
+    default:
+      fprintf(stderr, "Unknown flag: %c\n\n", c);
+      break;
+    }
+  }
+}
 
 int main(int argc, const char **argv) {
-  char cwd[4096];
-  getcwd(cwd, 4096);
+  flags_t flags = {
+      .hidden_files = false,
+  };
 
-  DIR *dir = opendir(cwd);
+  const char *path = "./";
+
+  for (int i = 1; i < argc; ++i) {
+    const char *arg = argv[i];
+
+    if (arg[0] == '-') {
+      evaluate_flags(&flags, arg);
+    } else {
+      path = arg;
+    }
+  }
+
+  char absolute_path[4096];
+  path = realpath(path, absolute_path);
+
+  DIR *dir = opendir(absolute_path);
   if (!dir) {
-    fprintf(stderr, "Failed to open current working directory!\n");
+    fprintf(stderr, "Path is not a directory: %s\n", absolute_path);
     return -1;
   }
 
@@ -17,10 +53,10 @@ int main(int argc, const char **argv) {
   struct dirent *entry;
   while ((entry = readdir(dir)) != NULL) {
     if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-      if (i != 0)
-        fputs(" | ", stdout);
+      if (!flags.hidden_files && entry->d_name[0] == '.')
+        continue;
 
-      printf("%s", entry->d_name);
+      printf("%s\n", entry->d_name);
     }
 
     ++i;
